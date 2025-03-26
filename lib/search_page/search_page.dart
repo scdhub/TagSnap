606407f33c0158ab0f234334e4f47bb9da0c3357
SCD20240805PC\SCD20240805 <koshima.mayu@scd.co.jp>
2025-03-26 18:09:29 +0900

    変更点 ・画面修正

---
+++ lib/search_page/search_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _SearchPage();
+}
+
+class _SearchPage extends State<SearchPage> with SingleTickerProviderStateMixin {
+  bool isReading = false;
+  // late TabController _tabController;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
