61198fd5cc1a32d55d970dd921bd40346ea26af0
SCD <namiki@scd.jp>
2025-04-16 11:12:10 +0900

    端末からのタグ情報読み取り処理追加

--- android/app/src/main/kotlin/com/example/tagsnap/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/tagsnap/MainActivity.kt
@@ -1,5 +1,129 @@
 package com.example.tagsnap
-
+import android.os.Message
+import android.os.Handler
+import android.os.Looper
+import com.rscja.deviceapi.RFIDWithUHFUART
+import com.rscja.deviceapi.entity.UHFTAGInfo
+import com.rscja.deviceapi.entity.InventoryParameter
+import com.rscja.deviceapi.interfaces.IUHFInventoryCallback
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.EventChannel
+import io.flutter.plugin.common.MethodChannel
+import java.util.*
 
-class MainActivity: FlutterActivity()
+// デバッグ用log出力
+import android.util.Log
+
+class MainActivity : FlutterActivity() {
+
+    // 通信用の変数
+    private var eventSink: EventChannel.EventSink? = null
+    private var isInit = false
+    private var isReading = false
+    private var rfid = RFIDWithUHFUART.getInstance()
+
+    // Dartファイル側との通信を行うための共通文言
+    private val channel = "com.example.tagsnap/DevChannel"
+    private val stream = "com.example.tagsnap/DevStream"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        val methodChannel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, channel)
+        methodChannel.setMethodCallHandler { call, result ->
+            when (call.method) {
+                "initRFID" -> {
+                    result.success(initRFID())
+                }
+
+                "startRFIDScan" -> {
+                    result.success(startRFIDScan())
+                }
+
+                "stopRFIDScan" -> {
+                    stopRFIDScanInternal()
+                    result.success(true)
+                }
+
+                "TermRFID" -> {
+                    termRFID()
+                    result.success(true)
+                }
+
+                else -> result.notImplemented()
+            }
+        }
+
+        EventChannel(flutterEngine.dartExecutor.binaryMessenger, stream)
+            .setStreamHandler(object : EventChannel.StreamHandler {
+                override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
+                    eventSink = events
+                }
+
+                override fun onCancel(arguments: Any?) {
+                    eventSink = null
+                }
+            })
+    }
+
+    // RFIDの初期化処理
+    private fun initRFID(): Boolean {
+        rfid = RFIDWithUHFUART.getInstance()
+        isInit = rfid.init(this)
+        return isInit
+    }
+
+    // RFIDの読み取り開始処理
+    private fun startRFIDScan(): Boolean {
+        //　初期化終わっていなかったら終了
+        if (!isInit) return false
+
+        rfid.setInventoryCallback(object : IUHFInventoryCallback {
+            override fun callback(uhftagInfo: UHFTAGInfo) {
+                val epc = uhftagInfo.getEPC()
+                // flutter側へepc情報を通知
+                // flutterの制約によりメインスレッドで必ず返さないといけない
+                runOnUiThread {
+                    eventSink?.success(epc)
+                    Log.d("Kotlin:MainActivity", "epc情報送信：$epc")
+                }
+            }
+        })
+
+        val param = InventoryParameter()
+        param.resultData = InventoryParameter.ResultData().setNeedPhase(false)
+
+        val started = rfid.startInventoryTag(param)
+
+        if (started) {
+            isReading = true
+            return true
+        } else {
+            return false
+        }
+    }
+
+    // RFIDの読み取り停止処理
+    private fun stopRFIDScanInternal() {
+        if (isReading) {
+            rfid.stopInventory()
+            isReading = false
+        }
+    }
+
+    // RFIDの終了処理
+    private fun termRFID() {
+        if (!isInit) {
+            rfid.free()
+            isInit = false
+        }
+    }
+
+    // 破棄時にも念のためRFIDの一通りの終了処理を行う
+    override fun onDestroy() {
+        stopRFIDScanInternal()
+        termRFID()
+        super.onDestroy()
+    }
+}
---
+++ lib/common_method/wrapper_device_lib.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/services.dart';
+
+// デバイス通信を行うための処理を隠ぺいするためのクラス
+class WrapperDeviceLib {
+
+  // 端末操作メソッドとの連携用チャンネル作成
+  static const _devChannel = MethodChannel('com.example.tagsnap/DevChannel');
+  static const _devStream = EventChannel('com.example.tagsnap/DevStream');
+
+  // RFID情報読み取り用
+  // 初期化
+  static Future<bool> initRFID() async {
+    return await _devChannel.invokeMethod<bool>('initRFID') ?? false;
+  }
+
+  // 読み取り開始
+  static Future<bool> startRFIDScan() async {
+    return await _devChannel.invokeMethod<bool>('startRFIDScan') ?? false;
+  }
+
+  // 読み取った情報の戻り値取得
+  static Stream<String> get epcStream {
+    return _devStream.receiveBroadcastStream().map((result) => result as String);
+  }
+
+  // 読み取り停止
+  static Future<bool> stopRFIDScan() async {
+    return await _devChannel.invokeMethod<bool>('stopRFIDScan') ?? false;
+  }
+
+  // 終了
+  static Future<bool> termRFID() async {
+    return await _devChannel.invokeMethod<bool>('TermRFID') ?? false;
+  }
+
+
+  // QRコード読み取り用（未）
+  bool callDevice2DLib(int setNum){
+    bool ret = true;
+
+    return ret;
+  }
+
+}
